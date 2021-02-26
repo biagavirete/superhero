701dc179ed000f1452ba8d82693a3be7f7cfc70c
biagavirete <biagavirete@gmail.com>
2021-02-26 20:45:32 -0300

    feat: add input from material ui for validation

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,29 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import * as CharactersActions from '../../store/ducks/characters/actions';
 
 import { Button } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
+
 
 const SearchInput = () => {
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<String>();
+
   const dispatch = useDispatch();
 
   const inputValue = useRef<HTMLInputElement>(null);
 
   const handleSubmit = () => {
-    dispatch(CharactersActions.loadCharactersRequest(inputValue?.current?.value))
+    if (!inputValue?.current?.value) {
+      setError(true);
+      setErrorMessage('Please type hero name');
+    } else {
+      dispatch(CharactersActions.loadCharactersRequest(inputValue?.current?.value))
+    }
   }
 
   return (
     <>
       <div className="main-container">
-        <div className="input-container">
-          <div className="input-field">
-            <input type="search" placeholder="Type here" ref={inputValue} />
-            <div className="underline"></div>
-          </div>
-        </div>
+        <TextField
+          error={error}
+          id="standard-error-helper-text"
+          label="Hero name"
+          placeholder="Type hero name"
+          helperText={errorMessage}
+          inputRef={inputValue}
+        />
       </div>
       <div className="button-container">
         <Button
