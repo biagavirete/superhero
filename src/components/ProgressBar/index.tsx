50f0cead43214f1710e5b4481927dc2946ded413
biagavirete <biagavirete@gmail.com>
2021-02-25 22:18:05 -0300

    feat: add progressbar from material-ui

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,27 +1,39 @@
-import { Container } from './styles'
+import { LinearProgress } from '@material-ui/core';
+import { makeStyles, createStyles, withStyles, Theme } from '@material-ui/core/styles';
 
-export const ProgressBar = (props: any) => {
+const BorderLinearProgress = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: 10,
+      width: 200,
+      borderRadius: 5,
+      margin: '0 0 5px 0'
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: '#e2c72d',
+    },
+  }),
+)(LinearProgress);
+
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function CustomizedProgressBars(props: any) {
   const { value } = props;
 
+  const classes = useStyles();
+
   return (
-    <Container>
-      <progress value={value} max={100} />
-    </Container>
-  )
-
-};
-
-// ProgressBar.propTypes = {
-//   value: PropTypes.number.isRequired,
-//   max: PropTypes.number,
-//   color: PropTypes.string,
-//   width: PropTypes.string,
-// };
-
-// ProgressBar.defaultProps = {
-//   max: 100,
-//   color: '#e2c72d',
-//   width: '10rem',
-// };
-
-export default ProgressBar;
+    <div className={classes.root}>
+      <BorderLinearProgress variant="determinate" value={value} />
+    </div>
+  );
+}
